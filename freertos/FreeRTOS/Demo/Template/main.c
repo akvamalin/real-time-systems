5a5ff52c1890a1910fe932f5e712687e986c472b
YevheniiMaliavka <eugene17green@gmail.com>
2018-02-08 15:39:33 +0100

    FreeRTOS: Enhance template with TaskCreateSafe function

--- freertos/FreeRTOS/Demo/Template/main.c
+++ freertos/FreeRTOS/Demo/Template/main.c
@@ -4,6 +4,14 @@
 
 #define TASK_STACK_SIZE 1024
 
+BaseType_t TaskCreateSafe(
+	TaskFunction_t pvTaskCode,
+	const char* const pcName,
+	unsigned short usStackDepth,
+	void* pvParameters,
+	UBaseType_t uxPriority,
+	TaskHandle_t *pxCreatedTask);
+
 void simpleTask(void* data) {
 	char* name = (char*)data;
 	while (1) {
@@ -13,9 +21,24 @@ void simpleTask(void* data) {
 
 int main() {
 	printf("Hello World Example");
-	xTaskCreate(simpleTask, (const char*) "simple task", TASK_STACK_SIZE, "This is my simple task", 1, NULL);
+	TaskCreateSafe(simpleTask, (const char*) "simple task", TASK_STACK_SIZE, "This is my simple task", 1, NULL);
 	vTaskStartScheduler();
 	return 0;
+}
+
+BaseType_t TaskCreateSafe(
+	TaskFunction_t pvTaskCode,
+	const char* const pcName,
+	unsigned short usStackDepth,
+	void* pvParameters,
+	UBaseType_t uxPriority,
+	TaskHandle_t *pxCreatedTask) {
+	BaseType_t status = xTaskCreate(pvTaskCode, pcName, usStackDepth, pvParameters, uxPriority, pxCreatedTask);
+	if (status == errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY) {
+		printf("Error: could not safely create a task > errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY");
+		exit(errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY);
+	}
+	return status;
 }
 
 void vAssertCalled(unsigned long ulLine, const char * const pcFileName){
