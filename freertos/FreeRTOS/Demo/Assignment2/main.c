3c08722584c550e9bae2b56cd680467f05b5f418
YevheniiMaliavka <eugene17green@gmail.com>
2018-02-15 13:04:36 +0100

    FreeRTOS: Use fixed priority scheduling

--- freertos/FreeRTOS/Demo/Assignment2/main.c
+++ freertos/FreeRTOS/Demo/Assignment2/main.c
@@ -1,15 +1,21 @@
 /*
 	Development of Real-Time-Systems
 	Module: Static Scheduling
+	Done here: Fixed Priority Scheduling
 	Assignee: Yevhenii Maliavka
 */
 
 #include <stdio.h>
 #include "FreeRTOS.h"
 #include "task.h"
-
 #define MATRIX_SIZE 20
 #define true 1
+
+TaskHandle_t communicationTaskHandler = NULL, matrixTaskHandler = NULL;
+int communicationTaskExecution = 0, 
+	communicationTaskCounter = 0, 
+	matrixTaskExecution = 0, 
+	matrixTaskCounter = 0;
 
 // TaskCreateSafe function definition
 // outputs error when creation was unsuccessful
@@ -59,6 +65,8 @@ double matrixesProductSum(double** a, double** b) {
 }
 
 void matrixTask() {
+	// Period of the communication Task: 100ms
+	int period = 100;
 	double** a = allocateMatrix();
 	double** b = allocateMatrix();
 	double** c = allocateMatrix();
@@ -74,23 +82,74 @@ void matrixTask() {
 				c[i][j] = matrixesProductSum(a, b);
 			}
 		}
-		printf("Calculated!\n");
-		vTaskDelay(100);
+		matrixTaskExecution = matrixTaskCounter;
+		printf("Calculated! Matrix Task Execution Time: %d\n", matrixTaskExecution);
+		matrixTaskCounter= 0;
+		vTaskDelay(period);
 	}
 }
 
 void communicationTask() {
+	// Period of the communication Task: 100ms + I/O operations 100ms = 200ms
+	int writingDuration = 100;
+	int period = 100;
 	while (true) {
 		printf("Sending data...\n");
-		vTaskDelay(100);
-		printf("Data sent!\n");
-		vTaskDelay(100);
+		vTaskDelay(writingDuration);
+		communicationTaskExecution = communicationTaskCounter + writingDuration;
+		printf("Data sent! Communication Task Execution Time: %d\n", communicationTaskExecution);
+		communicationTaskCounter = 0;
+		vTaskDelay(period);
 	}
 }
 
-void vApplicationTickHook(void)
-{
-	printf("Hook\n");
+void priorityManagerTask() {
+	// Set task phase to 210 so that the first conditions can be evaluated
+	// 200 Is the conditional value and 10 - extra ticks that may occur
+	vTaskDelay(210);
+	int lastCommunicationTaskPriority = 1;
+	while (true) {
+		if (communicationTaskExecution > 1000) {
+			printf("Increasing priority of Communication task\n");
+			vTaskPrioritySet(communicationTaskHandler, 4);
+			lastCommunicationTaskPriority = 4;
+		}
+		// so firstly we check if the execution time has already been measured
+		// then we check if the priority has not already been set to 2
+		// then we compare it to supposed 200ms execution time
+		else if (communicationTaskExecution != 0 &&
+				 lastCommunicationTaskPriority!= 2 &&
+				 communicationTaskExecution < 200) {
+			printf("Decreasing priority of Communication task\n");
+			vTaskPrioritySet(communicationTaskHandler, 2);
+			lastCommunicationTaskPriority = 2;
+		}
+		// Period
+		vTaskDelay(210);
+	}
+}
+
+void vApplicationTickHook(void){
+	TaskHandle_t currentTaskHandler = xTaskGetCurrentTaskHandle();
+	eTaskState communicationTaskState = eTaskGetState(communicationTaskHandler);
+	eTaskState matrixTaskState = eTaskGetState(matrixTaskHandler);
+
+	if (currentTaskHandler == matrixTaskHandler) {
+		if (communicationTaskState == eReady) {
+			// This means that matrix task has preempted the communication task
+			// and has put it into the READY state -> the communication task must wait
+			communicationTaskCounter++;
+		}
+		matrixTaskCounter++;
+	}
+	else if (currentTaskHandler == communicationTaskHandler) {
+		if (communicationTaskState == eReady) {
+			// This means that communication task has preempted the matrix task
+			// and has put it into the READY state -> the matrix task must wait
+			matrixTaskCounter++;
+		}
+		communicationTaskCounter++;
+	}
 }
 
 // Entry Point
@@ -98,12 +157,11 @@ int main() {
 	// xTaskCreate creates a new task and adds it to the list of tasks that are ready to run
 	// Each task requires RAM that is used to hold the task state, and used by the task as its stack.
 	// The required RAM is automatically allocated from the FreeRTOS heap
-
 	// NOTE! Low priority numbers denote low priority tasks
-
 	// Starts the RTOS scheduler. After calling the RTOS kernel has control over which tasks are executed and when.
-	//TaskCreateSafe(matrixTask, "MatrixTask", configMINIMAL_STACK_SIZE * 20, NULL, 3, NULL);
-	//TaskCreateSafe(communicationTask, "CommunicatioTask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+	TaskCreateSafe(priorityManagerTask, "PriorityManager", configMINIMAL_STACK_SIZE, NULL, 4, NULL);
+	TaskCreateSafe(matrixTask, "MatrixTask", configMINIMAL_STACK_SIZE * 20, NULL, 3, &matrixTaskHandler);
+	TaskCreateSafe(communicationTask, "CommunicatioTask", configMINIMAL_STACK_SIZE, NULL, 1, &communicationTaskHandler);
 	vTaskStartScheduler();
 	return 0;
 }
